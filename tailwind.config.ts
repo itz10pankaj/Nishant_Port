4689ddd71a25c7132c4abf744c60bd6a5b980d83
Pankaj <108193904+itz10pankaj@users.noreply.github.com>
2024-08-10 22:23:59 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,7 @@ const {
   default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
 
-const config = {
+module.exports= {
   darkMode: ["class"],
   content: [
     "./pages/**/*.{ts,tsx}",
